b5090552d9d1fc6de93393b0f74b291cad205e1e
Hariprasath_Karunakaran <136335795+harip666@users.noreply.github.com>
2025-12-26 10:28:46 +0530

    Added extra verification to the UserValidator logic

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -3,7 +3,8 @@ const joi = require("joi") ;
 const createUserSchema = joi.object({
   name: joi.string().min(2).required(),
   email: joi.string().email().required(),
-  age: joi.number().min(1)
+  age: joi.number().min(1),
+  password: joi.string().min(4).required()
 }) ;
 
 module.exports = { createUserSchema } ;
